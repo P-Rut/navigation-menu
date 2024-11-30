d884e49782978b0c11d72484bc5af4cb5d8d80b7
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-11-30 22:46:52 +0100

    feat: create NavigationList component to display list of navigation items

---
+++ src/app/components/navigation/NavigationList.jsx
@@ -0,0 +1,12 @@
+export default function NavigationList() {
+  return (
+    <div className="bg-white rounded-md p-5">
+      <h2 className="text-lg">Navigation placeholder</h2>
+      <ul className="list-disc">
+        <li>label 1</li>
+        <li>label 2</li>
+        <li>label 3</li>
+      </ul>
+    </div>
+  )
+}
