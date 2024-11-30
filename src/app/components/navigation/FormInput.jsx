fc2baeba1fab681dea68fbacb44be337748b1007
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-11-30 23:00:33 +0100

    feat: Create FormInput component to handle reusable inputs

---
+++ src/app/components/navigation/FormInput.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function FormInput({ label, placeholder }) {
+  return (
+    <div className="flex flex-col gap-1">
+      <label htmlFor="nameInput" className="text-sm font-medium text-[#344054]">
+        {label}
+      </label>
+      <input
+        type="text"
+        name="nameInput"
+        placeholder={placeholder}
+        className="w-full p-2 border border-[#D0D5DD] rounded-md text-sm placeholder:text-gray-400"
+      />
+    </div>
+  )
+}
